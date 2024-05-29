816dabb194bacdbf8c155d83a2095d0547f0a163
Jia Sheng | シェーン <66236001+jiasheng98@users.noreply.github.com>
2024-05-29 14:51:22 +0900

    Initial commit

---
+++ UI/composables/useVideoSetings.ts
@@ -0,0 +1,40 @@
+export interface VideoResultFormat {
+  url: string;
+  image: string;
+  videoUrl?: {
+    fileType: string;
+    link: string;
+    quality: string;
+  };
+}
+
+export const useVideoSettings = () => {
+    const video = useLocalStorage<{
+        script: string;
+        voice: string;
+        videoSubject: string;
+        extraPrompt: string;
+        search: string;
+        aiModel: string;
+        finalVideoUrl: string;
+        selectedAudio: string;
+        selectedVideoUrls: VideoResultFormat[];
+    }>('VideoSettings',{
+        script: "",
+        voice: "en_us_001",
+        videoSubject: "",
+        extraPrompt: "",
+        search: "",
+
+        aiModel: "g4f",
+
+        finalVideoUrl: "",
+        //   Audio related 
+
+        selectedAudio: "",
+        selectedVideoUrls: [],
+    });
+
+
+    return { video }
+}
